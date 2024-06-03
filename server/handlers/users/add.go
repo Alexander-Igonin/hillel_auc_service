8e50aa33423161220126ba3e90badba1757ba529
Alexander-Igonin <oleksandr.igonin@aura.com>
2024-06-03 22:01:51 +0300

    add part of hm

---
+++ server/handlers/users/add.go
@@ -0,0 +1,25 @@
+package users
+
+import (
+	"fmt"
+
+	"github.com/labstack/echo/v4"
+)
+
+type AddUserRequestBody struct {
+	NameFull string `json:"name"`
+	Email    string `json:"email"`
+	Phone    string `json:"phone"`
+}
+
+type AddUserResponseBody struct{}
+
+func AddUserHandler(ctx echo.Context) error {
+	user := AddUserRequestBody{}
+	err := ctx.Bind(&user)
+	if err != nil {
+		return fmt.Errorf("failed to bind user %w", err)
+	}
+
+	return nil
+}
